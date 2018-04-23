ea859f408d9d6a7c8062ebadfe1bff3a4f1a8004
Vaughn Vernon <vaughn@forcomprehension.com>
2018-04-23 00:28:38 -0700

    Improved test configuration reuse.

--- src/test/java/io/vlingo/auth/infra/resource/TenantResourceTest.java
+++ src/test/java/io/vlingo/auth/infra/resource/TenantResourceTest.java
@@ -7,49 +7,21 @@
 
 package io.vlingo.auth.infra.resource;
 
+import static io.vlingo.http.resource.serialization.JsonSerialization.deserialized;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.nio.ByteBuffer;
 import java.util.Properties;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import io.vlingo.actors.Definition;
-import io.vlingo.actors.World;
-import io.vlingo.actors.testkit.TestUntil;
-import io.vlingo.auth.infra.resource.TestResponseChannelConsumer.Progress;
-import io.vlingo.auth.model.Tenant;
 import io.vlingo.http.Response;
 import io.vlingo.http.ResponseHeader;
-import io.vlingo.http.resource.Resource;
-import io.vlingo.http.resource.ResourceHandler;
-import io.vlingo.http.resource.Resources;
-import io.vlingo.http.resource.Server;
-import static io.vlingo.http.resource.serialization.JsonSerialization.deserialized;
-import static io.vlingo.http.resource.serialization.JsonSerialization.serialized;
-import io.vlingo.wire.channel.ResponseChannelConsumer;
-import io.vlingo.wire.fdx.bidirectional.ClientRequestResponseChannel;
-import io.vlingo.wire.message.ByteBufferAllocator;
-import io.vlingo.wire.message.Converters;
-import io.vlingo.wire.node.Address;
-import io.vlingo.wire.node.AddressType;
-import io.vlingo.wire.node.Host;
 
-public class TenantResourceTest {
-  private ClientRequestResponseChannel client;
-  private ResponseChannelConsumer consumer;
-  private Progress progress;
-  protected Resource<?> resource;
-  protected Class<? extends ResourceHandler> resourceHandlerClass;
-  protected Resources resources;
-  private Server server;
-  protected World world;
+public class TenantResourceTest extends ResourceTest {
 
   @Test
   public void testThatTenantSubscribes() {
@@ -83,7 +55,7 @@ public class TenantResourceTest {
     
     final String location = createdResponse.headerOf(ResponseHeader.Location).value;
     
-    final Response patchTenantDeactive = this.patchTenantDeactivateRequestResponse(createdTenantData.tenantId);
+    final Response patchTenantDeactive = patchTenantDeactivateRequestResponse(createdTenantData.tenantId);
     assertEquals(Response.Ok, patchTenantDeactive.status);
     final TenantData deactivatedTenantData = deserialized(patchTenantDeactive.entity.content, TenantData.class);
     assertFalse(deactivatedTenantData.active);
@@ -204,193 +176,27 @@ public class TenantResourceTest {
     assertTrue(userRegistrateredData.active);
   }
 
-  @Before
-  public void setUp() throws Exception {
-    world = World.start("tenant-resource-test");
-
-    final Properties properties = new Properties();
-
-    properties.setProperty("server.http.port", "8080");
-    properties.setProperty("server.dispatcher.pool", "10");
-    properties.setProperty("server.buffer.pool.size", "2000");
-    properties.setProperty("server.message.buffer.size", "65535");
-    properties.setProperty("server.probe.interval", "10");
-    properties.setProperty("server.probe.timeout", "10");
-    properties.setProperty("server.request.missing.content.timeout", "100");
-
-    properties.setProperty("resource.name.tenant", "[subscribe, queryTenant, activate, deactivate, description, name, provisionGroup, provisionRole, registerUser]");
-    properties.setProperty("resource.tenant.handler", "io.vlingo.auth.infra.resource.TenantResource");
-    properties.setProperty("resource.tenant.pool", "10");
-    properties.setProperty("resource.tenant.disallowPathParametersWithSlash", "true");
-
-    properties.setProperty("action.tenant.subscribe.method", "POST");
-    properties.setProperty("action.tenant.subscribe.uri", "/tenants");
-    properties.setProperty("action.tenant.subscribe.to", "subscribeFor(body:io.vlingo.auth.infra.resource.TenantData tenantData)");
-    properties.setProperty("action.tenant.subscribe.permission", "io.vlingo.auth.TenantRepresentative");
-    
-    properties.setProperty("action.tenant.queryTenant.method", "GET");
-    properties.setProperty("action.tenant.queryTenant.uri", "/tenants/{tenantId}");
-    properties.setProperty("action.tenant.queryTenant.to", "queryTenant(String tenantId)");
-
-    properties.setProperty("action.tenant.activate.method", "PATCH");
-    properties.setProperty("action.tenant.activate.uri", "/tenants/{tenantId}/activate");
-    properties.setProperty("action.tenant.activate.to", "activate(String tenantId)");
-    properties.setProperty("action.tenant.activate.permission", "io.vlingo.auth.TenantRepresentative");
-
-    properties.setProperty("action.tenant.deactivate.method", "PATCH");
-    properties.setProperty("action.tenant.deactivate.uri", "/tenants/{tenantId}/deactivate");
-    properties.setProperty("action.tenant.deactivate.to", "deactivate(String tenantId)");
-    properties.setProperty("action.tenant.deactivate.permission", "io.vlingo.auth.TenantRepresentative");
-
-    properties.setProperty("action.tenant.description.method", "PATCH");
-    properties.setProperty("action.tenant.description.uri", "/tenants/{tenantId}/description");
-    properties.setProperty("action.tenant.description.to", "changeDescription(String tenantId, body:java.lang.String description)");
-    properties.setProperty("action.tenant.description.permission", "io.vlingo.auth.TenantRepresentative");
-    
-    properties.setProperty("action.tenant.name.method", "PATCH");
-    properties.setProperty("action.tenant.name.uri", "/tenants/{tenantId}/name");
-    properties.setProperty("action.tenant.name.to", "changeName(String tenantId, body:java.lang.String name)");
-    properties.setProperty("action.tenant.name.permission", "io.vlingo.auth.TenantRepresentative");
-    
-    properties.setProperty("action.tenant.provisionGroup.method", "POST");
-    properties.setProperty("action.tenant.provisionGroup.uri", "/tenants/{tenantId}/groups");
-    properties.setProperty("action.tenant.provisionGroup.to", "provisionGroup(String tenantId, body:io.vlingo.auth.infra.resource.GroupData groupData)");
-    properties.setProperty("action.tenant.provisionGroup.permission", "io.vlingo.auth.TenantRepresentative");
-    
-    properties.setProperty("action.tenant.provisionRole.method", "POST");
-    properties.setProperty("action.tenant.provisionRole.uri", "/tenants/{tenantId}/roles");
-    properties.setProperty("action.tenant.provisionRole.to", "provisionRole(String tenantId, body:io.vlingo.auth.infra.resource.RoleData roleData)");
-    properties.setProperty("action.tenant.provisionRole.permission", "io.vlingo.auth.TenantRepresentative");
-    
-    properties.setProperty("action.tenant.registerUser.method", "POST");
-    properties.setProperty("action.tenant.registerUser.uri", "/tenants/{tenantId}/users");
-    properties.setProperty("action.tenant.registerUser.to", "registerUser(String tenantId, body:io.vlingo.auth.infra.resource.UserRegistrationData userData)");
-    properties.setProperty("action.tenant.registerUser.permission", "io.vlingo.auth.TenantRepresentative");
-    
-    server = Server.startWith(world.stage(), properties);
-    Thread.sleep(10); // delay for server startup
-
-    progress = new Progress();
-
-    consumer = world.actorFor(Definition.has(TestResponseChannelConsumer.class, Definition.parameters(progress)), ResponseChannelConsumer.class);
-
-    client = new ClientRequestResponseChannel(Address.from(Host.of("localhost"), 8080, AddressType.NONE), consumer, 100, 10240, world.defaultLogger());
+  protected Properties resourceProperties() {
+    return TestProperties.tenantResourceProperties();
   }
 
-  @After
-  public void tearDown() {
-    client.close();
-
-    server.stop();
-
-    world.terminate();
-  }
-
-  private final ByteBuffer buffer = ByteBufferAllocator.allocate(65535);
-  
-  protected ByteBuffer toByteBuffer(final String requestContent) {
-    buffer.clear();
-    buffer.put(Converters.textToBytes(requestContent));
-    buffer.flip();
-    return buffer;
-  }
-
-  protected String patchActivateRequest(final String tenantId) {
-    return "PATCH /tenants/" + tenantId + "/activate HTTP/1.1\nHost: vlingo.io\n\n";
-  }
-
-  protected String patchDeactivateRequest(final String tenantId) {
-    return "PATCH /tenants/" + tenantId + "/deactivate HTTP/1.1\nHost: vlingo.io\n\n";
-  }
-
-  protected String postSubscribeRequest(final String body) {
-    return "POST /tenants HTTP/1.1\nHost: vlingo.io\nContent-Length: " + body.length() + "\n\n" + body;
-  }
-
-  protected String tenant() {
-    return serialized(tenantData());
-  }
-
-  protected TenantData tenantData() {
-    return tenantData("Test-Tenant", "Test-Tenant description.", true);
-  }
-
-  protected TenantData tenantData(final String name, final String description) {
-    return tenantData(name, description, true);
-  }
-
-  protected TenantData tenantData(final String name, final String description, boolean active) {
-    return TenantData.from(name, description, active);
-  }
-
-  protected TenantData tenantData(final Tenant tenant, final boolean withId) {
-    return TenantData.from(withId ? tenant.tenantId().value : null, tenant.name(), tenant.description(), tenant.isActive());
-  }
-
-  protected UserRegistrationData userRegistrationData(final String tenantId) {
-    return UserRegistrationData.from(
-            tenantId,
-            "useroftheyear",
-            ProfileData.from(PersonNameData.of("Given", "A", "Family"), "me@family.us", "212-555-1212"),
-            CredentialData.from("vlingo-platform", "useroftheyear", "topsecret"),
-            true);
-  }
-
-  protected Response getTenantRequestResponse(final String tenantLocation) {
-    final String request = "GET " + tenantLocation + " HTTP/1.1\nHost: vlingo.io\n\n";
-    return requestResponse(request);
-  }
-
-  protected Response patchTenantActivateRequestResponse(final String tenantId) {
+  private Response patchTenantActivateRequestResponse(final String tenantId) {
     final String request = "PATCH /tenants/" + tenantId + "/activate HTTP/1.1\nHost: vlingo.io\n\n";
     return requestResponse(request);
   }
 
-  protected Response patchTenantDeactivateRequestResponse(final String tenantId) {
+  private Response patchTenantDeactivateRequestResponse(final String tenantId) {
     final String request = "PATCH /tenants/" + tenantId + "/deactivate HTTP/1.1\nHost: vlingo.io\n\n";
     return requestResponse(request);
   }
 
-  protected Response patchTenantDescriptionRequestResponse(final String tenantId, final String description) {
+  private Response patchTenantDescriptionRequestResponse(final String tenantId, final String description) {
     final String request = "PATCH /tenants/" + tenantId + "/description HTTP/1.1\nHost: vlingo.io\nContent-Length: " + description.length() + "\n\n" + description;
     return requestResponse(request);
   }
 
-  protected Response patchTenantNameRequestResponse(final String tenantId, final String name) {
+  private Response patchTenantNameRequestResponse(final String tenantId, final String name) {
     final String request = "PATCH /tenants/" + tenantId + "/name HTTP/1.1\nHost: vlingo.io\nContent-Length: " + name.length() + "\n\n" + name;
     return requestResponse(request);
-  }
-
-  protected Response postProvisionGroup(final String tenantId, final String name, final String description) {
-    final String body = serialized(GroupData.from(name, description));
-    final String request = "POST /tenants/" + tenantId + "/groups HTTP/1.1\nHost: vlingo.io\nContent-Length: " + body.length() + "\n\n" + body;
-    return requestResponse(request);
-  }
-
-  protected Response postProvisionRole(final String tenantId, final String name, final String description) {
-    final String body = serialized(RoleData.from(name, description));
-    final String request = "POST /tenants/" + tenantId + "/roles HTTP/1.1\nHost: vlingo.io\nContent-Length: " + body.length() + "\n\n" + body;
-    return requestResponse(request);
-  }
-
-  protected Response postTenantSubscribesRequestResponse(final TenantData tenantToSubscribe) {
-    final String request = postSubscribeRequest(serialized(tenantToSubscribe));
-    return requestResponse(request);
-  }
-  
-  protected Response postRegisterUser(String tenantId, UserRegistrationData userRegData) {
-    final String body = serialized(userRegData);
-    final String request = "POST /tenants/" + tenantId + "/users HTTP/1.1\nHost: vlingo.io\nContent-Length: " + body.length() + "\n\n" + body;
-    return requestResponse(request);
-  }
-  
-  protected Response requestResponse(final String request) {
-    progress.untilConsumed = TestUntil.happenings(1);
-    client.requestWith(toByteBuffer(request));
-    while (progress.untilConsumed.remaining() > 0) {
-      client.probeChannel();
-    }
-    progress.untilConsumed.completes();
-    return progress.responses.poll();
   }
 }
