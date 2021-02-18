39d9663f9b5410194e85cace6cea845516b00d19
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-02-19 03:37:00 +0800

    prettier: Increase print width
    Signed-off-by: Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/main/frontend/prettier.config.js
+++ src/main/frontend/prettier.config.js
@@ -1,6 +1,7 @@
 /** @type {import('prettier').Options} */
 module.exports = {
 	tabWidth: 4,
+	printWidth: 100,
 	singleQuote: true,
 	plugins: ['./node_modules/prettier-plugin-svelte'],
 	svelteStrictMode: false,
