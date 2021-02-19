8510a7b6af61404c615b6b666f645bcfa3c24cfe
Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
2021-02-19 11:38:55 +0800

    Cleanup
    Signed-off-by: Don Alfons Nisnoni <donnisnoni.tid3@gmail.com>
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/main/frontend/src/routes/_layout.svelte
+++ src/main/frontend/src/routes/_layout.svelte
@@ -1,16 +1,12 @@
 <script>
 	import { Container, MaterialApp } from 'svelte-materialify/src';
 	import { isMobile, theme } from '../stores';
-
-	$: bgTheme = $theme === 'light' ? '#ffffff' : '#212121';
 </script>
 
 <svelte:window on:resize={isMobile.check} />
 
-<div style="background-color: {bgTheme}">
-	<MaterialApp theme={$theme}>
-		<Container class="ma-0">
-			<slot />
-		</Container>
-	</MaterialApp>
-</div>
+<MaterialApp theme={$theme}>
+	<Container>
+		<slot />
+	</Container>
+</MaterialApp>
