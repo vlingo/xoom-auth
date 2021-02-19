badb1a3bab7ad6cf2a9c78f540946043d51caebb
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-02-19 14:43:39 +0800

    Cleanup routes
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/main/frontend/src/util/routes.js
+++ src/main/frontend/src/util/routes.js
@@ -1,16 +1,10 @@
-import { mdiEllipse, mdiEllipseOutline, mdiFactory, mdiFileDocument, mdiFileDocumentOutline, mdiHome, mdiHomeOutline, mdiStore, mdiStoreOutline, mdiTag, mdiTagOutline } from '@mdi/js';
+import { mdiHome, mdiHomeOutline } from '@mdi/js';
 
 export default [
 	{
 		text: 'Home',
 		icon: mdiHomeOutline,
 		openIcon: mdiHome,
-		href: ''
-	},
-	{
-		text: 'Tenants',
-		icon: mdiFactory,
-		openIcon: mdiFactory,
-		href: '/admin/tenants'
+		href: '/admin'
 	},
 ];
