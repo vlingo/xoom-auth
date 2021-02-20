f8556b90fea978f503df4e766d7883ba88891153
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-02-20 16:16:48 +0800

    Tenant description table
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/main/frontend/src/routes/admin/tenants.svelte
+++ src/main/frontend/src/routes/admin/tenants.svelte
@@ -1,5 +1,31 @@
 <script>
-	//
+	let data = [
+		{
+			name: 'John Doe',
+			description: 'Description of the tenant',
+			active: true,
+		},
+		{
+			name: 'Kevin Wu',
+			description: 'Description of the tenant',
+			active: true,
+		},
+		{
+			name: 'Lisa Smit',
+			description: 'Description of the tenant',
+			active: true,
+		},
+		{
+			name: 'David Willish',
+			description: 'Description of the tenant',
+			active: true,
+		},
+		{
+			name: 'Samantha Bryan',
+			description: 'Description of the tenant',
+			active: true,
+		},
+	];
 </script>
 
 <svelte:head>
@@ -7,3 +33,25 @@
 </svelte:head>
 
 <h6>Tenants Subscribtion</h6>
+
+<div class="mt-5">
+	<table style="width:100%">
+		<thead>
+			<tr>
+				<td>Name</td>
+				<td>Description</td>
+				<td>Active</td>
+			</tr>
+		</thead>
+
+		<tbody>
+			{#each data as tenant}
+				<tr>
+					<td>{tenant.name}</td>
+					<td>{tenant.description}</td>
+					<td>{tenant.active}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
