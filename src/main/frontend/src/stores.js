bdb5f6e1b2e48650e916365771461244bf232c43
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-02-20 13:15:51 +0800

    Fix theme color have null value when there is no theme set on the localstorage
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/main/frontend/src/stores.js
+++ src/main/frontend/src/stores.js
@@ -1,7 +1,8 @@
 import { writable } from 'svelte/store';
 
 export function createLocalStore(key, initialValue) {
-	const localValue = process.browser ? localStorage.getItem(key) : initialValue;
+	const localValue =
+		process.browser && !!localStorage.getItem(key) ? localStorage.getItem(key) : initialValue;
 	const { subscribe, set } = writable(localValue);
 
 	return {
@@ -10,11 +11,10 @@ export function createLocalStore(key, initialValue) {
 			if (process.browser) {
 				localStorage.setItem(key, value);
 			}
-			set(value)
+			set(value);
 		},
 	};
 }
 
-
-export const isLoggedIn = writable(true)
-export const theme = createLocalStore('theme', 'light')
+export const isLoggedIn = writable(true);
+export const theme = createLocalStore('theme', 'light');
