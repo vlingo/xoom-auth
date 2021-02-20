a7db0dc4b2f3f0e52ccfb2a8358c647b18109b22
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-02-20 21:49:28 +0800

    Using svelte-materialify table component instead
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/main/frontend/src/routes/admin/tenants.svelte
+++ src/main/frontend/src/routes/admin/tenants.svelte
@@ -1,9 +1,14 @@
 <script>
+	import { mdiCheckboxBlank, mdiCheckboxMarked } from '@mdi/js';
+	import { Icon } from 'svelte-materialify/src';
+
+	import { Table } from 'svelte-materialify/src';
+
 	let data = [
 		{
 			name: 'John Doe',
 			description: 'Description of the tenant',
-			active: true,
+			active: false,
 		},
 		{
 			name: 'Kevin Wu',
@@ -13,7 +18,7 @@
 		{
 			name: 'Lisa Smit',
 			description: 'Description of the tenant',
-			active: true,
+			active: false,
 		},
 		{
 			name: 'David Willish',
@@ -23,7 +28,7 @@
 		{
 			name: 'Samantha Bryan',
 			description: 'Description of the tenant',
-			active: true,
+			active: false,
 		},
 	];
 </script>
@@ -35,12 +40,32 @@
 <h6>Tenants Subscribtion</h6>
 
 <div class="mt-5">
-	<table style="width:100%">
+	<Table class="p-5 s-card">
 		<thead>
 			<tr>
 				<td>Name</td>
 				<td>Description</td>
-				<td>Active</td>
+				<td class="text-center">Active</td>
+			</tr>
+		</thead>
+		<tbody>
+			{#each data as tenant}
+				<tr>
+					<td>{tenant.name}</td>
+					<td>{tenant.description}</td>
+					<td class="text-center">
+						<Icon path={tenant.active ? mdiCheckboxMarked : mdiCheckboxBlank} />
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</Table>
+	<!-- <table style="width:100%">
+		<thead>
+			<tr>
+				<td>Name</td>
+				<td>Description</td>
+				<td class="text-center">Active</td>
 			</tr>
 		</thead>
 
@@ -49,9 +74,11 @@
 				<tr>
 					<td>{tenant.name}</td>
 					<td>{tenant.description}</td>
-					<td>{tenant.active}</td>
+					<td class="text-center">
+						<Icon path={tenant.active ? mdiCheckboxMarked : mdiCheckboxBlank} />
+					</td>
 				</tr>
 			{/each}
 		</tbody>
-	</table>
+	</table> -->
 </div>
