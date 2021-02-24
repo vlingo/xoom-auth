97ede212166e0022c57f78b7f2d0689dc19c2389
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-02-24 16:12:09 +0800

    Cleanup
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/main/frontend/src/components/alerts/OrganizationAlert.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	import Alert from './Alert.svelte';
-	export let notChosenAlert = false;
-</script>
-
-<Alert name="an Organization" link={!notChosenAlert ? "organization" : ""} />
--- src/main/frontend/src/components/alerts/VersionAlert.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	import Alert from "./Alert.svelte";
-	export let notChosenAlert = false;
-</script>
-
-<Alert name="a Schema Version" link={!notChosenAlert ? "schemaVersion" : ""} />
