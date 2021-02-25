1bf1aef6cdee62cff4c76cf5ddf688fff2709ccd
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-02-25 13:02:21 +0800

    Disable service worker for now
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/main/frontend/rollup.config.js
+++ src/main/frontend/rollup.config.js
@@ -107,20 +107,20 @@ export default {
 		onwarn,
 	},
 
-	serviceworker: !dev && {
-		input: config.serviceworker.input(),
-		output: config.serviceworker.output(),
-		plugins: [
-			resolve(),
-			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode),
-			}),
-			commonjs(),
-			!dev && terser(),
-		],
+	// serviceworker: !dev && {
+	// 	input: config.serviceworker.input(),
+	// 	output: config.serviceworker.output(),
+	// 	plugins: [
+	// 		resolve(),
+	// 		replace({
+	// 			'process.browser': true,
+	// 			'process.env.NODE_ENV': JSON.stringify(mode),
+	// 		}),
+	// 		commonjs(),
+	// 		!dev && terser(),
+	// 	],
 
-		preserveEntrySignatures: false,
-		onwarn,
-	},
+	// 	preserveEntrySignatures: false,
+	// 	onwarn,
+	// },
 };
