da054e263049bd1354ad2902ee50348317863cd9
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-02-27 17:26:44 +0800

    Improve dialog overflow
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/main/frontend/src/components/CommonDialog.svelte
+++ src/main/frontend/src/components/CommonDialog.svelte
@@ -1,6 +1,12 @@
+<script context="module">
+	let id = 1;
+</script>
+
 <script>
-	import { Button, Dialog, Divider } from 'svelte-materialify/src';
+	import { Button, Dialog } from 'svelte-materialify/src';
 	import { createEventDispatcher } from 'svelte';
+
+	id++;
 
 	const emit = createEventDispatcher();
 
@@ -11,19 +17,28 @@
 	export let submitButtonCaptionOnLoading = '';
 </script>
 
-<Dialog class="pa-4" bind:active>
-	<form on:submit|preventDefault={() => emit('form-submit')} class="d-flex flex-column">
-		<h6 class="mb-2">{title}</h6>
+<Dialog class="d-flex flex-column" bind:active>
+	<h6 class="pa-2">{title}</h6>
+	<!-- style="height:100%;max-height:100%;overflow-y:auto" -->
+	<form
+		on:submit|preventDefault={() => emit('form-submit')}
+		id="dialog-form-{id}"
+		class="d-flex flex-column pa-2 w-full h-full max-h-full overflow-y-auto">
 		<slot />
-		<div class="mt-3 d-flex">
-			<Button depressed on:click={() => (active = false)}>cancel</Button>
-			<Button class="ml-auto primary-color" name="button" disabled={loading} type="submit">
-				{#if loading}
-					{submitButtonCaptionOnLoading}
-				{:else}
-					{submitButtonCaption}
-				{/if}
-			</Button>
-		</div>
 	</form>
+	<div class="mt-3 pa-2 d-flex">
+		<Button depressed on:click={() => (active = false)}>cancel</Button>
+		<Button
+			class="ml-auto primary-color"
+			form="dialog-form-{id}"
+			name="button"
+			disabled={loading}
+			type="submit">
+			{#if loading}
+				{submitButtonCaptionOnLoading}
+			{:else}
+				{submitButtonCaption}
+			{/if}
+		</Button>
+	</div>
 </Dialog>
