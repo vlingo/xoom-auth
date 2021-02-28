0760111dd436b80e060001131f1e39360cd2d04f
Don Alfons Nisnoni <donnisnoni@outlook.com>
2021-02-28 19:31:42 +0800

    Increase common dialog padding; so it consistent with the delete dialog
    Signed-off-by: Don Alfons Nisnoni <donnisnoni@outlook.com>

--- src/main/frontend/src/components/CommonDialog.svelte
+++ src/main/frontend/src/components/CommonDialog.svelte
@@ -18,15 +18,15 @@
 </script>
 
 <Dialog class="d-flex flex-column" bind:active>
-	<h6 class="pa-2">{title}</h6>
+	<h6 class="pa-4">{title}</h6>
 	<!-- style="height:100%;max-height:100%;overflow-y:auto" -->
 	<form
 		on:submit|preventDefault={() => emit('form-submit')}
 		id="dialog-form-{id}"
-		class="d-flex flex-column pa-2 w-full h-full max-h-full overflow-y-auto">
+		class="d-flex flex-column pa-4 w-full h-full max-h-full overflow-y-auto">
 		<slot />
 	</form>
-	<div class="mt-3 pa-2 d-flex">
+	<div class="mt-3 pa-4 d-flex">
 		<Button depressed on:click={() => (active = false)}>cancel</Button>
 		<Button
 			class="ml-auto primary-color"
