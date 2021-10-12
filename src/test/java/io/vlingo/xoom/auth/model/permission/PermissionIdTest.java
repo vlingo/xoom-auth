6a02ccc5dd280ef21352f7a8b8eedd6fd55b8772
Jakub Zalas <jakub@zalas.pl>
2021-10-12 20:36:27 +0100

    Improve test examples
    Signed-off-by: Jakub Zalas <jakub@zalas.pl>

--- src/test/java/io/vlingo/xoom/auth/model/permission/PermissionIdTest.java
+++ src/test/java/io/vlingo/xoom/auth/model/permission/PermissionIdTest.java
@@ -8,19 +8,19 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class PermissionIdTest {
   @Test
   public void itIsComposedOfTenantIdAndPermissionId() {
-    final PermissionId permissionId = PermissionId.from("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", "2cfa9bfb-6cdc-47b6-b155-16877dc2cdf6");
+    final PermissionId permissionId = PermissionId.from("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", "permission-a");
 
     assertEquals("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", permissionId.tenantId);
-    assertEquals("2cfa9bfb-6cdc-47b6-b155-16877dc2cdf6", permissionId.permissionName);
-    assertEquals("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be:2cfa9bfb-6cdc-47b6-b155-16877dc2cdf6", permissionId.idString());
+    assertEquals("permission-a", permissionId.permissionName);
+    assertEquals("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be:permission-a", permissionId.idString());
   }
 
   @Test
   public void twoPermissionIdsAreTheSameIfTenantIdAndPermissionIdAreTheSame() {
-    final PermissionId permissionId = PermissionId.from("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", "2cfa9bfb-6cdc-47b6-b155-16877dc2cdf6");
+    final PermissionId permissionId = PermissionId.from("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", "permission-a");
 
-    assertEquals(permissionId, PermissionId.from("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", "2cfa9bfb-6cdc-47b6-b155-16877dc2cdf6"));
-    assertNotEquals(permissionId, PermissionId.from("97d57df1-1b38-4583-bd3c-6b4731e7a605", "2cfa9bfb-6cdc-47b6-b155-16877dc2cdf6"));
-    assertNotEquals(permissionId, PermissionId.from("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", "0428d634-f81b-4952-8886-fd15a34292f0"));
+    assertEquals(permissionId, PermissionId.from("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", "permission-a"));
+    assertNotEquals(permissionId, PermissionId.from("97d57df1-1b38-4583-bd3c-6b4731e7a605", "permission-a"));
+    assertNotEquals(permissionId, PermissionId.from("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", "permission-b"));
   }
 }
--- src/test/java/io/vlingo/xoom/auth/model/role/RoleIdTest.java
+++ src/test/java/io/vlingo/xoom/auth/model/role/RoleIdTest.java
@@ -8,19 +8,19 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 public class RoleIdTest {
   @Test
   public void itIsComposedOfTenantIdAndRoleId() {
-    final RoleId roleId = RoleId.from("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", "2cfa9bfb-6cdc-47b6-b155-16877dc2cdf6");
+    final RoleId roleId = RoleId.from("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", "role-a");
 
     assertEquals("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", roleId.tenantId);
-    assertEquals("2cfa9bfb-6cdc-47b6-b155-16877dc2cdf6", roleId.roleName);
-    assertEquals("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be:2cfa9bfb-6cdc-47b6-b155-16877dc2cdf6", roleId.idString());
+    assertEquals("role-a", roleId.roleName);
+    assertEquals("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be:role-a", roleId.idString());
   }
 
   @Test
   public void twoRoleIdsAreTheSameIfTenantIdAndRoleIdAreTheSame() {
-    final RoleId roleId = RoleId.from("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", "2cfa9bfb-6cdc-47b6-b155-16877dc2cdf6");
+    final RoleId roleId = RoleId.from("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", "role-a");
 
-    assertEquals(roleId, RoleId.from("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", "2cfa9bfb-6cdc-47b6-b155-16877dc2cdf6"));
-    assertNotEquals(roleId, RoleId.from("97d57df1-1b38-4583-bd3c-6b4731e7a605", "2cfa9bfb-6cdc-47b6-b155-16877dc2cdf6"));
-    assertNotEquals(roleId, RoleId.from("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", "0428d634-f81b-4952-8886-fd15a34292f0"));
+    assertEquals(roleId, RoleId.from("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", "role-a"));
+    assertNotEquals(roleId, RoleId.from("97d57df1-1b38-4583-bd3c-6b4731e7a605", "role-a"));
+    assertNotEquals(roleId, RoleId.from("c60317e9-cf4f-408a-9fd1-8e0f4e69d2be", "role-b"));
   }
 }
