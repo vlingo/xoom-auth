127443e56d9f98d494cc60b1cb5d40939a3e5b3d
Jakub Zalas <jakub@zalas.pl>
2021-10-20 20:47:04 +0200

    Avoid await() in RoleEntityTest
    Signed-off-by: Jakub Zalas <jakub@zalas.pl>

--- src/test/java/io/vlingo/xoom/auth/model/role/RoleEntityTest.java
+++ src/test/java/io/vlingo/xoom/auth/model/role/RoleEntityTest.java
@@ -4,6 +4,7 @@ import io.vlingo.xoom.actors.World;
 import io.vlingo.xoom.actors.testkit.AccessSafely;
 import io.vlingo.xoom.auth.infrastructure.persistence.*;
 import io.vlingo.xoom.auth.model.tenant.TenantId;
+import io.vlingo.xoom.common.Completes;
 import io.vlingo.xoom.lattice.model.sourcing.SourcedTypeRegistry;
 import io.vlingo.xoom.lattice.model.sourcing.SourcedTypeRegistry.Info;
 import io.vlingo.xoom.symbio.EntryAdapterProvider;
@@ -14,6 +15,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 
+import static io.vlingo.xoom.auth.test.Assertions.assertCompletes;
 import static io.vlingo.xoom.auth.test.Assertions.assertEventDispatched;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -25,18 +27,16 @@ public class RoleEntityTest {
   private final RoleId ROLE_ID = RoleId.from(TENANT_ID, ROLE_NAME);
 
   private World world;
-  private Journal<String> journal;
   private MockDispatcher dispatcher;
 
   @BeforeEach
   @SuppressWarnings({"unchecked", "rawtypes"})
-  public void setUp(){
+  public void setUp() {
     world = World.startWithDefaults("test-es");
 
     dispatcher = new MockDispatcher();
 
-    EntryAdapterProvider entryAdapterProvider = EntryAdapterProvider.instance(world);
-
+    final EntryAdapterProvider entryAdapterProvider = EntryAdapterProvider.instance(world);
     entryAdapterProvider.registerAdapter(RoleProvisioned.class, new RoleProvisionedAdapter());
     entryAdapterProvider.registerAdapter(RoleDescriptionChanged.class, new RoleDescriptionChangedAdapter());
     entryAdapterProvider.registerAdapter(GroupAssignedToRole.class, new GroupAssignedToRoleAdapter());
@@ -46,122 +46,131 @@ public class RoleEntityTest {
     entryAdapterProvider.registerAdapter(RolePermissionAttached.class, new RolePermissionAttachedAdapter());
     entryAdapterProvider.registerAdapter(RolePermissionDetached.class, new RolePermissionDetachedAdapter());
 
-    journal = world.actorFor(Journal.class, InMemoryJournalActor.class, Collections.singletonList(dispatcher));
+    final Journal<String> journal = world.actorFor(Journal.class, InMemoryJournalActor.class, Collections.singletonList(dispatcher));
 
     new SourcedTypeRegistry(world).register(new Info(journal, RoleEntity.class, RoleEntity.class.getSimpleName()));
   }
 
   @Test
-  public void provisionRole() {
+  public void roleIsProvisionedForTheTenant() {
     final AccessSafely dispatcherAccess = dispatcher.afterCompleting(1);
-    final RoleState state = roleOf(ROLE_ID).provisionRole(ROLE_NAME, ROLE_DESCRIPTION).await();
+    final Completes<RoleState> outcome = roleOf(ROLE_ID).provisionRole(ROLE_NAME, ROLE_DESCRIPTION);
 
-    assertEquals(ROLE_ID, state.roleId);
-    assertEquals(ROLE_NAME, state.name);
-    assertEquals(ROLE_DESCRIPTION, state.description);
-    assertEventDispatched(dispatcherAccess, 1, RoleProvisioned.class);
+    assertCompletes(outcome, state -> {
+      assertEquals(ROLE_ID, state.roleId);
+      assertEquals(ROLE_NAME, state.name);
+      assertEquals(ROLE_DESCRIPTION, state.description);
+      assertEventDispatched(dispatcherAccess, 1, RoleProvisioned.class);
+    });
   }
 
   @Test
-  public void changeDescription() {
+  public void roleDescriptionIsChanged() {
     final AccessSafely dispatcherAccess = dispatcher.afterCompleting(1);
 
-    givenRoleExists(ROLE_ID);
+    final Completes<RoleState> outcome = givenRoleExists(ROLE_ID)
+            .andThenTo(r -> roleOf(ROLE_ID).changeDescription("updated-role-description"));
 
-    final RoleState state = roleOf(ROLE_ID).changeDescription("updated-role-description").await();
-
-    assertEquals(ROLE_ID, state.roleId);
-    assertEquals(ROLE_NAME, state.name);
-    assertEquals("updated-role-description", state.description);
-    assertEventDispatched(dispatcherAccess, 2, RoleDescriptionChanged.class);
+    assertCompletes(outcome, state -> {
+      assertEquals(ROLE_ID, state.roleId);
+      assertEquals(ROLE_NAME, state.name);
+      assertEquals("updated-role-description", state.description);
+      assertEventDispatched(dispatcherAccess, 2, RoleDescriptionChanged.class);
+    });
   }
 
   @Test
-  public void assignGroup() {
+  public void groupIsAssignedToRole() {
     final AccessSafely dispatcherAccess = dispatcher.afterCompleting(1);
 
-    givenRoleExists(ROLE_ID);
+    final Completes<RoleState> outcome = givenRoleExists(ROLE_ID)
+            .andThenTo(r -> roleOf(ROLE_ID).assignGroup("updated-role-name"));
 
-    final RoleState state = roleOf(ROLE_ID).assignGroup("updated-role-name").await();
-
-    assertEquals(ROLE_ID, state.roleId);
-    assertEquals("updated-role-name", state.name);
-    assertEquals(ROLE_DESCRIPTION, state.description);
-    assertEventDispatched(dispatcherAccess, 2, GroupAssignedToRole.class);
+    assertCompletes(outcome, state -> {
+      assertEquals(ROLE_ID, state.roleId);
+      assertEquals("updated-role-name", state.name);
+      assertEquals(ROLE_DESCRIPTION, state.description);
+      assertEventDispatched(dispatcherAccess, 2, GroupAssignedToRole.class);
+    });
   }
 
   @Test
-  public void unassignGroup() {
+  public void groupIsUnassignedFromRole() {
     final AccessSafely dispatcherAccess = dispatcher.afterCompleting(1);
 
-    givenRoleExists(ROLE_ID);
+    final Completes<RoleState> outcome = givenRoleExists(ROLE_ID)
+            .andThenTo(r -> roleOf(ROLE_ID).unassignGroup("updated-role-name"));
 
-    final RoleState state = roleOf(ROLE_ID).unassignGroup("updated-role-name").await();
-
-    assertEquals(ROLE_ID, state.roleId);
-    assertEquals("updated-role-name", state.name);
-    assertEquals(ROLE_DESCRIPTION, state.description);
-    assertEventDispatched(dispatcherAccess, 2, GroupUnassignedFromRole.class);
+    assertCompletes(outcome, state -> {
+      assertEquals(ROLE_ID, state.roleId);
+      assertEquals("updated-role-name", state.name);
+      assertEquals(ROLE_DESCRIPTION, state.description);
+      assertEventDispatched(dispatcherAccess, 2, GroupUnassignedFromRole.class);
+    });
   }
 
   @Test
-  public void assignUser() {
+  public void userIsAssignedToRole() {
     final AccessSafely dispatcherAccess = dispatcher.afterCompleting(1);
 
-    givenRoleExists(ROLE_ID);
+    final Completes<RoleState> outcome = givenRoleExists(ROLE_ID)
+            .andThenTo(r -> roleOf(ROLE_ID).assignUser("updated-role-name"));
 
-    final RoleState state = roleOf(ROLE_ID).assignUser("updated-role-name").await();
-
-    assertEquals(ROLE_ID, state.roleId);
-    assertEquals("updated-role-name", state.name);
-    assertEquals(ROLE_DESCRIPTION, state.description);
-    assertEventDispatched(dispatcherAccess, 2, UserAssignedToRole.class);
+    assertCompletes(outcome, state -> {
+      assertEquals(ROLE_ID, state.roleId);
+      assertEquals("updated-role-name", state.name);
+      assertEquals(ROLE_DESCRIPTION, state.description);
+      assertEventDispatched(dispatcherAccess, 2, UserAssignedToRole.class);
+    });
   }
 
   @Test
-  public void unassignUser() {
+  public void userIsUnassignedFromRole() {
     final AccessSafely dispatcherAccess = dispatcher.afterCompleting(1);
 
-    givenRoleExists(ROLE_ID);
+    final Completes<RoleState> outcome = givenRoleExists(ROLE_ID)
+            .andThenTo(r -> roleOf(ROLE_ID).unassignUser("updated-role-name"));
 
-    final RoleState state = roleOf(ROLE_ID).unassignUser("updated-role-name").await();
-
-    assertEquals(ROLE_ID, state.roleId);
-    assertEquals("updated-role-name", state.name);
-    assertEquals(ROLE_DESCRIPTION, state.description);
-    assertEventDispatched(dispatcherAccess, 2, UserUnassignedFromRole.class);
+    assertCompletes(outcome, state -> {
+      assertEquals(ROLE_ID, state.roleId);
+      assertEquals("updated-role-name", state.name);
+      assertEquals(ROLE_DESCRIPTION, state.description);
+      assertEventDispatched(dispatcherAccess, 2, UserUnassignedFromRole.class);
+    });
   }
 
   @Test
-  public void attach() {
+  public void permissionIsAttachedToRole() {
     final AccessSafely dispatcherAccess = dispatcher.afterCompleting(1);
 
-    givenRoleExists(ROLE_ID);
+    final Completes<RoleState> outcome = givenRoleExists(ROLE_ID)
+            .andThenTo(r -> roleOf(ROLE_ID).attach("updated-role-name"));
 
-    final RoleState state = roleOf(ROLE_ID).attach("updated-role-name").await();
-
-    assertEquals(ROLE_ID, state.roleId);
-    assertEquals("updated-role-name", state.name);
-    assertEquals(ROLE_DESCRIPTION, state.description);
-    assertEventDispatched(dispatcherAccess, 2, RolePermissionAttached.class);
+    assertCompletes(outcome, state -> {
+      assertEquals(ROLE_ID, state.roleId);
+      assertEquals("updated-role-name", state.name);
+      assertEquals(ROLE_DESCRIPTION, state.description);
+      assertEventDispatched(dispatcherAccess, 2, RolePermissionAttached.class);
+    });
   }
 
   @Test
-  public void detach() {
+  public void permissionIsDetachedFromRole() {
     final AccessSafely dispatcherAccess = dispatcher.afterCompleting(1);
 
-    givenRoleExists(ROLE_ID);
+    final Completes<RoleState> outcome = givenRoleExists(ROLE_ID)
+            .andThenTo(state -> roleOf(ROLE_ID).detach("updated-role-name"));
 
-    final RoleState state = roleOf(ROLE_ID).detach("updated-role-name").await();
-
-    assertEquals(ROLE_ID, state.roleId);
-    assertEquals("updated-role-name", state.name);
-    assertEquals(ROLE_DESCRIPTION, state.description);
-    assertEventDispatched(dispatcherAccess, 2, RolePermissionDetached.class);
+    assertCompletes(outcome, state -> {
+      assertEquals(ROLE_ID, state.roleId);
+      assertEquals("updated-role-name", state.name);
+      assertEquals(ROLE_DESCRIPTION, state.description);
+      assertEventDispatched(dispatcherAccess, 2, RolePermissionDetached.class);
+    });
   }
 
-  private void givenRoleExists(RoleId roleId) {
-    roleOf(roleId).provisionRole(ROLE_NAME, ROLE_DESCRIPTION).await();
+  private Completes<RoleState> givenRoleExists(RoleId roleId) {
+    return roleOf(roleId).provisionRole(ROLE_NAME, ROLE_DESCRIPTION);
   }
 
   private Role roleOf(RoleId roleId) {
